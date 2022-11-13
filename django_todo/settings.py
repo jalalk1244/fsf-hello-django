8b0edf691665599cc425ed45fc805cb43e4e3c0c
jalalk1244 <jalaluddin.boss@gmail.com>
2022-11-13 11:46:15 +0000

    Prepared to deploy to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 if os.path.exists('env.py'):
     import env
@@ -77,11 +78,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://gadhrjabuxcehz:d68804cb3d533dd0cef73250cf84258a43bc1abe386971676ea8bca0a092bda9@ec2-52-3-200-138.compute-1.amazonaws.com:5432/d3ovdmapmprtv2')
 }
 
 
--- todo/test_views.py
+++ todo/test_views.py
@@ -38,7 +38,7 @@ class TestViews(TestCase):
         updated_item = Item.objects.get(id=item.id)
         # self.assertEqual(updated_item.done, False)
         self.assertFalse(updated_item.done)
-    
+
     def test_can_edit_item(self):
         item = Item.objects.create(name='Test Todo Item')
         response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
